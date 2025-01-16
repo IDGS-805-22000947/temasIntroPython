958c35af06f1b4d6709720dc74b6b2b280eced01
Damian Lopez <damislop04@gmail.com>
2025-01-16 16:22:30 -0600

    Commit del dia 14 de enero

---
+++ tem2_entradas.py
@@ -0,0 +1,22 @@
+
+'''
+print("Dame dos números: ")
+num1=input('Num1: ')
+num2=input('Num2: ')
+suma=int(num1)+int(num2)
+print("La suma de los dos números es: ", suma)
+print("La suma de {} + {} es: {}" .format(num1,num2,suma))
+
+'''
+
+def tabla():
+    # Solicitar el número al usuario
+    numero = int(input("Dame un numero: "))
+
+    
+    # Generar e imprimir la tabla de multiplicar
+    for i in range(1, 11):
+        print(f"{i} x {numero} = {i * numero}")
+
+# Llamar a la función
+tabla()
---
+++ tem4_while.py
@@ -0,0 +1,20 @@
+
+
+x=0
+
+While x<=10
+	print(x)
+	x=x+1
+
+ 
+while True: 
+
+
+
+
+
+##Ejercicio
+##pedir un numero y que haga su tabla de multiplicar del 1-10
+
+print("Dame el numero para hacer su tabla de multiplicar")
+num1=input('Numero: ')
